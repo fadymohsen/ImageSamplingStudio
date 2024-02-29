a084407a146951dfaeef6ab5f09145109706d76f
engfadymohsen <82896079+fadymohsen@users.noreply.github.com>
2024-02-29 19:13:58 +0200

    Exit mode

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 import sys
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QMainWindow, QTabWidget
 from PyQt5 import uic
+
+
+
+
+
 
 class MyTabWidget(QTabWidget):
     def __init__(self, ui_file):
         super().__init__()
         uic.loadUi(ui_file, self)
+
+    def keyPressEvent(self, event):
+        if event.key() == 16777216:  # Integer value for Qt.Key_Escape
+            if self.isFullScreen():
+                self.showNormal()  # Show in normal mode
+            else:
+                self.showFullScreen()  # Show in full screen
+        else:
+            super().keyPressEvent(event)
+
+
+
+
+
 
 def main():
     app = QApplication(sys.argv)
